15b380faac5375ab017bd626b8a3a60d9963e755
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-20 12:50:07 +0100

    Revert "Return result to server"
    This reverts commit e533563c01a24f20ed9a2b0e0ff6d492d0bafe6c.

--- AI/server.py
+++ AI/server.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask import Flask, render_template
 from main import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', evaluate(text))
+    evaluate(text)
+    return 'Evaluation'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
