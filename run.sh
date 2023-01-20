863b6b570ed3c771259e098352f37270d092cafd
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-20 15:06:04 +0100

    CSS

---
+++ run.sh
@@ -0,0 +1,2 @@
+clear
+python3 server.py
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from main import *
 
 app = Flask(__name__)
@@ -7,9 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/evaluate', methods = ['POST'])
+@app.route("/evaluate", methods=['GET', 'POST'])
 def evaluation():
-    return render_template("evaluate.html", result=evaluate(text))
+    if request.method == 'POST':
+        return render_template("evaluate.html", result=evaluate(request.form['text-area']))
+
+    elif request.method == 'GET':
+        print("No Post Back Call")
+
+    return render_template("index.html")
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
